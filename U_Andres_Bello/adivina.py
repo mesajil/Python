2a228bc15d2a1ca9ccc81ea68dda5be5e02aa33a
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-10-11 21:44:49 -0500

    adivina

---
+++ U_Andres_Bello/adivina.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == '__main__':
+    
+    # Fase de configuración
+
+    print ("BIENVENIDO AL JUEGO DE ADIVINA - PALABRA\n")
+    nombre1 = input("Ingrese nombre del jugador 1: ")
+    nombre2 = input("Ingrese nombre del jugador 2: ")
+    npalabras = int (input ("Ingrese cantidad de palabras a adivinar por jugador: "))
+    max_intentos = int (input ("Ingrese cantidad máxima de intentos para adivinar cada palabra: "))
+
