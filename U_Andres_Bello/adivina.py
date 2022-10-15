c7d4780fd946b681528e41ac43144f26988fc97e
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-10-14 22:13:47 -0500

    def

--- U_Andres_Bello/adivina.py
+++ U_Andres_Bello/adivina.py
@@ -1,4 +1,54 @@
+def obtener_palabra_a_adivinar ():
 
+    for _ in range(3):
+        palabra = input ("Ingrese palabra a adivinar: ")
+
+        # Verificamos que el tamaño de la palabra sea menor o igual a 20
+
+        if (len(palabra) > 20):
+            continue
+
+        # Verificamos que cada carácter en la cadena sea minúscula y pertenezca al alfabeto español.
+
+        palabra_esp = True
+        
+        for caracter in palabra:
+            if caracter not in "abcdefghijklmnñopqrstuvwxyz":
+                palabra_esp = False
+                break
+        
+        if (not palabra_esp):
+            continue 
+
+        # Se retorna la palabra
+
+        return palabra
+
+    # No se ingreso una palabra correctamente
+
+    return 0
+
+
+def adivinar_palabra (palabra, max_intentos):
+
+    letras_unicas = set(palabra)
+    letras_encontradas = set()
+
+    for i in range(max_intentos):
+
+        letra = (input("Ingrese letra: "))[0]
+
+        # Econtrar posiciones de letra en palabra
+
+        if (letra in palabra):
+            posiciones = encontrar_posiciones(letra, palabra)
+        
+        # Retornar puntaje si se adivinó la palabra
+
+        if (len(letras_encontradas) == len(letras_unicas)):
+            # Retorna puntaje
+    
+    return 0
 
 if __name__ == '__main__':
     
@@ -18,50 +68,35 @@ if __name__ == '__main__':
         
         print ("\nAhora juega Proponedor:", nombre1)
 
-        palabra_correcta = False
-
-        for _ in range(3):
-            palabra = input ("Ingrese palabra a adivinar: ")
-
-            # Verificamos que el tamaño de la palabra sea menor o igual a 20
-
-            if (len(palabra) > 20):
-                continue
-
-            # Verificamos que cada carácter en la cadena sea minúscula y pertenezca al alfabeto español.
-
-            palabra_esp = True
-            
-            for caracter in palabra:
-                if caracter not in "abcdefghijklmnñopqrstuvwxyz":
-                    palabra_esp = False
-                    break
-            
-            if (not palabra_esp):
-                continue 
-
-            # Salimos del bucle for
-
-            palabra_correcta = True
-            break;
+        palabra = obtener_palabra_a_adivinar()
 
         # ¿Jugador 1 perdio el juego?
 
-        if (not palabra_correcta):
+        if (palabra == 0):
             break
+
+        # Turno adivinador jugador 2
+
+        print ("\nAhora juega Adivinador:", nombre2)
 
         # Turno proponedor jugador 2
         
         print ("\nAhora juega Proponedor:", nombre2)
 
-        # ...
+        palabra = obtener_palabra_a_adivinar()
 
         # ¿Jugador 2 perdio el juego?
 
-        # ...
+        if (palabra == 0):
+            break
 
+        # Turno adivinador jugador 2
 
+        print ("\nAhora juega Adivinador:", nombre1)
 
+    
+
+    
 
 
 
