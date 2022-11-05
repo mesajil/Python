e31a98b3b9515f8215c08f5994288aba98b52a15
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-11-04 22:24:48 -0500

    3

--- U/3/preg3.py
+++ U/3/preg3.py
@@ -1,27 +1,44 @@
 from random import randint
 from functools import reduce
 
-def generar_matriz (N, M):
-    return [[randint(0,20) for _ in range(M)] for _ in range(N)]
+def generar_matriz ():
+    
+    N = int(input("ingrese N de la Matriz: "))
+    M = int(input("ingrese M de la Matriz: "))
+
+    matriz = [[randint(0,20) for _ in range(M)] for _ in range(N)]
+
+    imprimir(matriz)
+    return matriz, N, M
 
 
 def imprimir(matriz):
-    
-    f1 = lambda a,b: f"{a}\n{b}"
-    f2 = lambda a,b: f"{a}\t{b}"
-    print(reduce(f1, [reduce(f2, row) for row in matriz]))
+    print()
+    print("\n".join(["\t".join([str(x) for x in row]) for row in matriz]))
     
 
-def guardar_matriz ():
-    pass
+def guardar_matriz (matriz, N, M):
+
+    path = rf"D:\Github\Python\U\3\matriz_{N}_por_{M}.txt"
+    with open(path, "w") as f:
+        for r in matriz:
+            f.write(",".join([str(x) for x in r]) + "\n")
+    print(f"\nGuardado OK del Archivo --> matriz_{N}_por_{M}.txt")
+
+
+def leer_matriz (N, M):
+    print(f"\nInicio Lectura de Archivo: matriz_{N}_por_{M}.txt")
+    matriz = []
+    path = rf"D:\Github\Python\U\3\matriz_{N}_por_{M}.txt"
+    with open(path, "r") as f:
+        for x in f:
+            row = [int(x) for x in x.split("\n")[0].split(",")]
+            matriz.append(row)
+
+    imprimir(matriz)
+    print(f"\nFinLectura de Archivo: matriz_{N}_por_{M}.txt")
 
 if __name__ == "__main__":
-    N = int(input("ingrese N de la Matriz: "))
-    M = int(input("ingrese M de la Matriz: "))
-    matriz = generar_matriz(N, M)
-    imprimir(matriz)
-    print("\nFin de Lectura")
-    guardar_matriz (matriz)
-    print(f"Guardado OK del Archivo --> matriz_{N}_por_{M}.txt")
-    matriz = leer_matriz ()
-    imprimir(matriz)
+    matriz, N, M = generar_matriz()
+    guardar_matriz (matriz, N, M)
+    leer_matriz (N, M)
