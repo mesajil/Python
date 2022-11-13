5ba02d447dcfa6e2db5ae9af69fb6944f04e2892
Luis Mesajil <lhmesajil@gmail.com>
2022-11-13 15:44:56 -0500

    rickandmorty

--- tools/downsub/text.py
+++ tools/downsub/text.py
@@ -1,8 +1,26 @@
 
 
-path = r'subs.txt'
-with open(path, 'r') as f:
-    text = ' '.join([line.replace('\n', '') for line in f if line != '\n'])
-print (text)
-with open('text.txt', 'w') as f:
-    f.write(text)
+def get_data ():
+    path = r'subs.txt'
+    with open(path, 'r') as f:
+        text = ' '.join([line.replace('\n', '') for line in f if line != '\n'])
+        return text
+
+
+def save_data(data):
+    path = r'out.txt'
+    with open(path, 'w') as f:
+        if isinstance(data, list):
+            f.writelines(data)
+        elif isinstance(data, str):
+            f.write(data)
+
+
+def main():
+    data = get_data()
+    # print(data)
+    save_data(data)
+
+
+if __name__ == '__main__':
+    main()
