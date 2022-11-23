4ffeff09606efc6f3b29f4ff54e46b86c42f4eda
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-11-23 00:53:33 -0500

    5

--- U/TA2022/TA2022.py
+++ U/TA2022/TA2022.py
@@ -65,7 +65,95 @@ def codificacion_alumno ():
 
 
 def juego_preguntas():
-    pass
+    class Pregunta:
+        def __init__ (self, pregunta, alternativas, respuesta):
+            self.pregunta = pregunta
+            self.alternativas = alternativas # Lista con las alternativas
+            self.respuesta = respuesta # Indice de la lista de alternativas
+
+        def obtener_respuesta(self):
+            # Devuelve el elemento de la lista correcto
+            return self.alternativas[self.respuesta - 1]
+    
+    print ("Juego de preguntas")
+    preguntas = [] # Lista de objetos Pregunta()
+    preguntas.append(Pregunta(
+        "Cuantos litros de sangre tiene una persona adulta?",
+        ["Tiene entre 2 y 4 litros", "Tiene entre 4 y 6 litros"],
+        2
+    ))
+    preguntas.append(Pregunta(
+        "Quien es el autor de la frase \"Pienso, luego existo\"?",
+        ["Descartes", "Socrates"],
+        1
+    ))
+    preguntas.append(Pregunta(
+        "Cual es el pais mas grande y el mas pequeno del mundo?",
+        ["China y Nauru", "Rusia y Vaticano"],
+        2
+    ))
+    preguntas.append(Pregunta(
+        "Cual es el libro mas vendido en el mundo despues de la Biblia?",
+        ["Don Quijote de la Mancha", "La Odisea"],
+        1
+    ))
+    preguntas.append(Pregunta(
+        "La sal comun esta formada por dos elementos, cuales son?",
+        ["Sodio y carbono", "Sodio y cloro"],
+        2
+    ))
+    preguntas.append(Pregunta(
+        "Quien pinto la obra \"Guernica\"?",
+        ["Salvador Dali", "Pablo Picasso"],
+        2
+    ))
+    preguntas.append(Pregunta(
+        "Cuanto tiempo tarda la luz del Sol en llegar a la Tierra?",
+        ["12 horas", "8 minutos"],
+        2
+    ))
+    preguntas.append(Pregunta(
+        "Cual es la nacionalidad de Jorge Mario Bergoglio?",
+        ["Mexicana", "Argentina"],
+        2
+    ))
+    preguntas.append(Pregunta(
+        "En que periodo de la prehistoria fue descubierto el fuego?",
+        ["Neolitico", "Paleolitico"],
+        2
+    ))
+    preguntas.append(Pregunta(
+        "A quien se le atribuye la frase \"Solo se que no se nada\"?",
+        ["Socrates", "Aristoteles"],
+        1
+    ))
+
+    puntaje = 0 # Puntaje del jugador
+    for i,p in enumerate(preguntas):
+        print (f"\n{i + 1}. {p.pregunta}")
+        print (f"1. {p.alternativas[0]}") # Primera alternativa
+        print (f"2. {p.alternativas[1]}") # Segunda alternativa
+        opcion = int(input("Elegir alternativa: "))
+        if opcion == p.respuesta:
+            print("Correcto!")
+            puntaje += 1
+        else:
+            print(f"Incorrecto, la respuesta correcta es: {p.obtener_respuesta()}")
+    
+    print ("\nResultados:")
+    print (f"Correctas:     {puntaje}")
+    print (f"Incorrectas:   {len(preguntas) - puntaje}")
+    
+    if puntaje <= 4:
+        print ("Nivel Alcanzado: Deficiente")
+    elif puntaje == 5:
+        print ("Nivel Alcanzado: Regular")
+    elif puntaje <= 8:
+        print ("Nivel Alcanzado: Bueno")
+    else:
+        print ("Nivel Alcanzado: Excelente")
+
+    input("\nPresione enter para continuar.")
 
 
 def juego_numeros ():
