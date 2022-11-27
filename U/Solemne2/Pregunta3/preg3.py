6e14778154082960c461426f17724ee0976571db
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-11-27 01:57:43 -0500

    p3

--- U/Solemne2/Pregunta3/preg3.py
+++ U/Solemne2/Pregunta3/preg3.py
@@ -1,31 +1,38 @@
 from random import randint
 
-def generar_matriz ():
-    
+def main():
+
     N = int(input("ingrese N de la Matriz: "))
     M = int(input("ingrese M de la Matriz: "))
 
-    return [[randint(0,20) for _ in range(M)] for _ in range(N)], N, M
+    matriz = [[randint(0,20) for _ in range(M)] for _ in range(N)]
+    
+    imprimir_matriz(matriz)
+    guardar_matriz (matriz, N, M)
+    leer_matriz (N, M)
 
 
 def imprimir_matriz(matriz):
-    
+    """Imprime la matriz con formato"""
+
     print("\nLa matriz es la siguiente:\n")
-    print("\n".join(["\t".join([str(x) for x in row]) for row in matriz]))
+    print("\n".join(["\t".join([str(e) for e in row]) for row in matriz]))
     
 
-def guardar_matriz (matriz):
-    
-    N = len(matriz)
-    M = len(matriz[0])
+def guardar_matriz (matriz, N, M):
+    """Guarda la matriz en un archivo externo"""
     
     path = rf"matriz_{N}_por_{M}.txt"
     with open(path, "w") as f:
-        f.writelines([",".join([str(x) for x in row]) + "\n" for row in matriz])
+        """Cada fila de la matriz se guarda en cada
+        linea del archivo"""
+        f.writelines([",".join([str(e) for e in row]) + "\n" for row in matriz])
     print(f"\nGuardado OK del Archivo ---> matriz_{N}_por_{M}.txt")
 
 
 def leer_matriz (N, M):
+    """Lee la matriz desde un archivo externo"""
+
     print(f"\nInicio Lectura de Archivo: matriz_{N}_por_{M}.txt")
     
     path = rf"matriz_{N}_por_{M}.txt"
@@ -34,13 +41,6 @@ def leer_matriz (N, M):
 
     imprimir_matriz(matriz)
     print(f"\nFin Lectura de Archivo: matriz_{N}_por_{M}.txt")
-
-
-def main():
-    matriz, N, M = generar_matriz()
-    imprimir_matriz(matriz)
-    guardar_matriz (matriz)
-    leer_matriz (N, M)
 
 
 if __name__ == "__main__":
