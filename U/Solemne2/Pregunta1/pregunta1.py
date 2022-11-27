760938e185f454ca24789421ac0c277a4642daa5
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-11-26 23:14:01 -0500

    preg1

--- U/Solemne2/Pregunta1/pregunta1.py
+++ U/Solemne2/Pregunta1/pregunta1.py
@@ -1,27 +1,43 @@
 """
-Solemne 02 Pregunta 1
-
-
+Pregunta 1
 """
 
-####################################################
-#           Funcion Crear venta
-####################################################
+
+def main():
+    
+    menu = """
+    MENU
+
+    [1] Nueva venta de pasaje
+    [2] Reporte de pasajes vendidos
+    [3] Salir del programa
+
+        Ingrese opcion: """ # Texto del menu
+    
+    while (1):
+        opcion = input(menu)
+        match opcion:
+            case "1": crear_venta()
+            case "2": reporte()
+            case "3": break
+
 
 def crear_venta():
     
-    # Lectura de datos
+    # Leer ciudades
     with open(r"ciudades.txt", "r") as f:
         ciudades = [line.split("\n")[0] for line in f]
+        print(ciudades)
     
-    # Proceso
+    # Formateamos las ciudades para mostrarlas al usuario
     formato = [f"{i+1}:{c}" for i,c in enumerate(ciudades)]
     
-    # Entradas
+    # Entrada de datos
     nombre = input("Nombre: ")
     apellido = input("Apellido: ")
     nacimiento = input("Fecha de nacimiento: ")
     while (1):
+        """Pedimos los indices de las ciudades de origen y destino al usuario"""
         origen = int(input(f"Seleccione Ciudad origen: {formato}: ")) - 1
         destino = int(input(f"Seleccione Ciudad destino: {formato}: ")) - 1
         if  valida_origendestino(origen, destino, ciudades):
@@ -29,32 +45,28 @@ def crear_venta():
     fecha = input("Fecha: ")
     hora = input("Hora de salida: ")
 
-    # Escribir datos
+    # Guardar datos de usuario
     datos = nombre, apellido,nacimiento,ciudades[origen],ciudades[destino],fecha,hora
     with open(r"ventas.txt", "a") as f:
-        f.write(','.join(datos) + '\n')
+        f.write(','.join(datos) + '\n') # Se guardan en una linea separados por comas
 
-
-####################################################
-#           Funcion validar origen y destino
-####################################################
 
 def valida_origendestino(origen, destino, ciudades):
-    # Salidas
+    """Validamos que las indices de las ciudades esten en un rango
+    y no sean iguales"""
     return origen in range(len(ciudades)) and destino in range(len(ciudades)) and origen != destino
 
 
-####################################################
-#           Funcion Reporte de pasajes vendidos
-####################################################
-
 def reporte():
 
-    # Leer datos -> ventas
+    # Leer ventas
     with open(r"ventas.txt", "r") as f:
+        """ventas es una lista de listas: cada elemento de "ventas"
+        es una lista de datos por cada venta.
+        Cada linea de texto es una cadena de datos separados por comas."""
         ventas = [line.split(",") for line in f]
     
-    # Salidas
+    # Imprimir ventas
     for v in ventas:
         print(f"""
     Datos del pasajero:
@@ -72,40 +84,5 @@ def reporte():
     """)
 
 
-####################################################
-#           Funcion principal
-####################################################
-
-def main():
-    
-    # Inicializar
-    menu = """
-    MENU
-
-    [1] Nueva venta de pasaje
-    [2] Reporte de pasajes vendidos
-    [3] Salir del programa
-
-        Ingrese opcion: """
-    
-    while (1):
-        
-        # Entradas
-        opcion = input(menu)
-        
-        # Proceso
-        match opcion:
-            case "1": crear_venta()
-            case "2": reporte()
-            case "3": break
-
-    # Salida: Fin del programa
-
-
-####################################################
-#           Punto de entrada
-####################################################
-
 if __name__ == "__main__":
     main()
-    # Salida: Fin del programa
