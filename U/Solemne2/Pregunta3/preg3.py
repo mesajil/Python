60eda39e64ef5e3e3fe6c044caa208d015648cb5
Luis Mesajil <lhmesajil@gmail.com>
2022-12-07 21:06:58 -0500

    matriz

--- U/Solemne2/Pregunta3/preg3.py
+++ U/Solemne2/Pregunta3/preg3.py
@@ -1,42 +1,43 @@
 from random import randint
 
-def main():
 
-    # Definir matriz
-    N = int(input("ingrese N de la Matriz: "))
-    M = int(input("ingrese M de la Matriz: "))
-    matriz = [[randint(0,20) for _ in range(M)] for _ in range(N)]
+# Generar matriz
+N = int(input("ingrese N de la Matriz: "))
+M = int(input("ingrese M de la Matriz: "))
+# matriz = [[randint(0,20) for _ in range(M)] for _ in range(N)]
+matriz = []
+for _ in range(N):
+    fila = []
+    for _ in range(M):
+        fila.append(randint(0,20))
+    matriz.append(fila)
 
-    # Imprimir la matriz sin corchetes
-    print("\nLa matriz es la siguiente:\n")    
-    # print("\n".join(["\t".join([str(e) for e in row]) for row in matriz])) # Utilizando list comprehension
-    # for row in matriz: # Utilizando condicionales
-    #     for i in range(len(row)):
-    #         if i < len(row) - 1:
-    #             print(row[i], end='\t')
-    #         else:
-    #             print(row[i])
+# Imprimir la matriz sin corchetes
+print("\nLa matriz es la siguiente:\n")
+# formato = "\n".join(["\t".join([str(e) for e in fila]) for fila in matriz]
+formato = ""
+for fila in matriz: 
+    for i in range(len(fila)):
+        formato += str(fila[i]) + '\t'
+    formato += '\n'
+print(formato)
 
-    for row in matriz: # Utilizando join
-        print("\t".join([str(e) for e in row]))
+# Guardar la matriz en un archivo externo
+path = rf"matriz_{N}_por_{M}.txt"
+with open(path, "w") as f:
+    """Cada fila de la matriz se guarda en cada
+    linea del archivo separando sus datos con comas"""
+    # f.writelines([",".join([str(e) for e in fila]) + "\n" for fila in matriz])
+    f.write (formato)
+print(f"\nGuardado OK del Archivo ---> matriz_{N}_por_{M}.txt")
 
-    # Guardar la matriz en un archivo externo
-    path = rf"matriz_{N}_por_{M}.txt"
-    with open(path, "w") as f:
-        """Cada fila de la matriz se guarda en cada
-        linea del archivo separando sus datos con comas"""
-        f.writelines([",".join([str(e) for e in row]) + "\n" for row in matriz])
-    print(f"\nGuardado OK del Archivo ---> matriz_{N}_por_{M}.txt")
+# Leer la matriz desde un archivo externo
+print(f"\nInicio Lectura de Archivo: matriz_{N}_por_{M}.txt\n")
+with open(path, "r") as f:
+    # matriz = [line.split("\n")[0].split(",") for line in f]
+    matriz = [line.replace('\n', '').split("\t") for line in f]
 
-    # Leer la matriz desde un archivo externo
-    print(f"\nInicio Lectura de Archivo: matriz_{N}_por_{M}.txt\n")
-    with open(path, "r") as f:
-        matriz = [line.split("\n")[0].split(",") for line in f]
-    
-    # Imprimir la matriz leida con formato
-    print("\n".join(["\t".join([str(e) for e in row]) for row in matriz]))
-    print(f"\nFin Lectura de Archivo: matriz_{N}_por_{M}.txt")
+# Imprimir la matriz leida con formato
+print("\n".join(["\t".join([str(e) for e in fila]) for fila in matriz]))
+print(f"\nFin Lectura de Archivo: matriz_{N}_por_{M}.txt")
 
-
-if __name__ == "__main__":
-    main()
